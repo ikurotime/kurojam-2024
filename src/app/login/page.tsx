ade35f1e005b5cbe34fb9cc36ff1a60797636435
David Huertas <davidhuertasortiz@gmail.com>
2024-05-15 02:50:08 +0200

    feat: Add login with Github button to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <div className='min-h-dvh flex flex-col w-full items-center justify-center'>
       <form className='p-12 flex flex-col bg-stone-950 max-w-md rounded-md border border-primary-bg'>
+        {/* @ts-ignore -- Typing conflict between next and html, needs check*/}
         <button formAction={login} className='text-xl'>
           Login with Github
         </button>
